471c31353c7df4635a567e7f17d8184e906e9cb7
HackMEAny <hackme.any@protonmail.com>
2021-10-24 02:21:28 +0530

    powermenu fixed rofi

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -92,6 +92,8 @@ case $chosen in
 				bspc quit
 			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
 				i3-msg exit
+			# elif [[ "$DESKTOP_SESSION" == "qtile" ]]; then
+			# 	mod4
 			fi
 		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
 			exit 0
