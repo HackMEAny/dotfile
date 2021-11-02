4cce15488ec828638b94bf75afde0f7ebc19b8df
HackMEAny <hackme.any@protonmail.com>
2021-11-03 00:14:41 +0530

    Rofi powermenu icon fixed

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -5,7 +5,7 @@ dir="$HOME/.config/rofi/powermenu"
 
 # random colors
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
-color="${styles[$(( $RANDOM % 8 ))]}"
+color="${styles[$(($RANDOM % 8))]}"
 
 # comment this line to disable random colors
 sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
@@ -19,18 +19,15 @@ uptime=$(uptime -p | sed -e 's/up //g')
 rofi_command="rofi -theme $dir/$theme"
 
 # Options
-shutdown=""
-reboot=""
-lock=""
-suspend=""
-logout=""
+shutdown=" ⏻ "
+reboot=" ﰇ "
+lock="  "
+suspend="  "
+logout="  "
 
 # Confirmation
 confirm_exit() {
-	rofi -dmenu\
-		-i\
-		-no-fixed-num-lines\
-		-p "Are You Sure? : "\
+	rofi -dmenu -i -no-fixed-num-lines -p "Are You Sure? : " \
 		-theme $dir/confirm.rasi
 }
 
@@ -44,61 +41,61 @@ options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
 case $chosen in
-    $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $lock)
-		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		elif [[ -f /usr/bin/betterlockscreen ]]; then
-			betterlockscreen -l
+$shutdown)
+	ans=$(confirm_exit &)
+	if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		systemctl poweroff
+	elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+		exit 0
+	else
+		msg
+	fi
+	;;
+$reboot)
+	ans=$(confirm_exit &)
+	if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		systemctl reboot
+	elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+		exit 0
+	else
+		msg
+	fi
+	;;
+$lock)
+	if [[ -f /usr/bin/i3lock ]]; then
+		i3lock
+	elif [[ -f /usr/bin/betterlockscreen ]]; then
+		betterlockscreen -l
+	fi
+	;;
+$suspend)
+	ans=$(confirm_exit &)
+	if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+	elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+		exit 0
+	else
+		msg
+	fi
+	;;
+$logout)
+	ans=$(confirm_exit &)
+	if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+			openbox --exit
+		elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+			i3-msg exit
+		# elif [[ "$DESKTOP_SESSION" == "qtile" ]]; then
+		# 	mod4
 		fi
-        ;;
-    $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			# elif [[ "$DESKTOP_SESSION" == "qtile" ]]; then
-			# 	mod4
-			fi
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
+	elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+		exit 0
+	else
+		msg
+	fi
+	;;
 esac
